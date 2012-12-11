8b20474ccb2e9a17f02eb29c56fef77a4028a7bf
cburkey <cburkey@entermediasoftware.com>
2012-12-11 15:57:55 -0500

    Added git save

---
+++ webapp/WEB-INF/bin/linux/gitaddrepository.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+localpath=$1
+url=$2
+checkoutpath=$3
+
+mkdir -p $localpath
+cd $localpath
+git --bare init
+git config core.sharedrepository 1
+git config receive.denyNonFastforwards true
+
+#
+cd $checkoutpath
+git clone $url
