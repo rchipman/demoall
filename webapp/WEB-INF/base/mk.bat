19feba8a5c7076f4aa2eb1b18b7e7d0b1049b44f
Chris Rose <chris.rose@openedit.org>
2014-01-31 11:59:01 -0500

    Additional Symbolic Links for Windows mk.bat script

--- webapp/WEB-INF/base/mk.bat
+++ webapp/WEB-INF/base/mk.bat
@@ -4,3 +4,5 @@ mklink /D system ..\..\..\..\entermedia-server\webapp\WEB-INF\base\system
 mklink /D manager ..\..\..\..\entermedia-server\webapp\WEB-INF\base\manager
 mklink /D emshare ..\..\..\..\app-emshare\webapp\emshare
 mklink /D openedit ..\..\..\..\extension-openedit\webapp\openedit
+mklink /D themes ..\..\..\..\entermedia-server\webapp\WEB-INF\base\themes
+mklink /D ..\bin\windows ..\..\..\..\installer_windows\webapp\WEB-INF\bin\windows
